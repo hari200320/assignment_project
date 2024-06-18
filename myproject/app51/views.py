731998288f2a4991592af3d1a0f95e28be58360e
Hari <hariharanr200318@gmail.com>
2024-06-18 20:56:43 +0530

    completed app51

---
+++ myproject/app51/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+import math
+
+def calculate(request):
+    n1 = 5
+    square = n1 * n1
+    factorial = math.factorial(n1)
+    context = {
+        'number': n1,
+        'square': square,
+        'factorial': factorial
+    }
+    return render(request, 'result.html', context)
